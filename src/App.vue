0ad786e05eb34b6e3256386d086a1767eb16f3f5
yanlibo <yanlibo@kedabeijing.com>
2018-11-23 17:26:35 +0800

    create branch dev

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">
-    <banner />
+    <div id="app">
+        <!-- <banner />
     <grid />
     <block />
-    <list />
-  </div>
+    <list /> -->
+        <div class="test "></div>
+        <div class=" ignore"></div>
+    </div>
 </template>
 
 <script>
@@ -14,77 +16,99 @@ import Block from '@/components/Block';
 import List from '@/components/List';
 
 export default {
-  name: 'App',
-  components: {
-    Banner,
-    Grid,
-    Block,
-    List
-  }
+    name: 'App',
+    components: {
+        Banner,
+        Grid,
+        Block,
+        List
+    }
 };
 </script>
 
 <style>
 html {
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    box-sizing: border-box;
 }
+
 body {
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-  background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+    background-color: #f4f4f4;
 }
+
+.test {
+    width: 100px;
+    height: 700px;
+    background: red
+}
+
+.ignore {
+    width: 100px;
+    height: 700px;
+    background: blue
+}
+
 * {
-  box-sizing: inherit;
-
-  &:before,
-  &:after {
     box-sizing: inherit;
-  }
-}
-img {
-  width: 100%;
-  height: auto;
-  vertical-align: top;
-  content: normal !important;
-}
-  [aspectratio] {
-	  position: relative;
-  }
-  [aspectratio]::before {
-	  content: '';
-	  display: block;
-	  width: 1px;
-	  margin-left: -1px;
-	  height: 0;
-  }
 
-  [aspectratio-content] {
-	  position: absolute;
-	  top: 0;
-	  left: 0;
-	  right: 0;
-	  bottom: 0;
-	  width: 100%;
-	  height: 100%;
-  }
-  [flexContainer] {
+    &:before,
+    &:after {
+        box-sizing: inherit;
+    }
+}
+
+img {
+    width: 100%;
+    height: auto;
+    vertical-align: top;
+    content: normal !important;
+}
+
+[aspectratio] {
+    position: relative;
+}
+
+[aspectratio]::before {
+    content: '';
+    display: block;
+    width: 1px;
+    margin-left: -1px;
+    height: 0;
+}
+
+[aspectratio-content] {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+}
+
+[flexContainer] {
     display: flex;
     width: 750px;
-  }
-  .block {
+}
+
+.block {
     margin-top: 15px;
-  }
-  ul,li {
+}
+
+ul,
+li {
     list-style: none outside none;
     margin: 0;
     padding: 0;
-  }
-  figure {
+}
+
+figure {
     display: block;
     margin: 0;
-  }
+}
 </style>
